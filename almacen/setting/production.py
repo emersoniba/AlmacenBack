be824939bf0e5d00713e4f589f386917ef8290cf
emersoniba <emersonadv2025@gmail.com>
2026-04-15 21:41:17 -0400

    archivos adicionales pra poder subitr a produccion 8

--- almacen/setting/production.py
+++ almacen/setting/production.py
@@ -5,12 +5,15 @@ import os
 DEBUG = False
 
 # Base de datos para producción
-import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME_PROD', 'almacen_db_prod'),
+        'USER': os.getenv('DB_USER_PROD', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD_PROD', '1234'),
+        'HOST': os.getenv('DB_HOST_PROD', ''),
+        'PORT': os.getenv('DB_PORT_PROD', '5432'),
+    }
 }
 # ============================================
 # CORRECCIÓN: ALLOWED_HOSTS para Render
@@ -39,7 +42,7 @@ CORS_ALLOWED_ORIGINS = [origin.strip() for origin in cors_origins.split(',')]
 CORS_ALLOWED_ORIGINS.append('https://*.onrender.com')
 
 # Security settings para producción
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
